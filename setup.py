a8ccc96932a0bcddc3f9f11c1a5f457ae125591c
Aayush shah <23053738@kiit.ac.in>
2026-08-01 12:16:49 +0530

    add the cookicutter templet

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A ml project to predict the mental health score on the scale of 1 to 10',
+    author='aayush',
+    license='',
+)
